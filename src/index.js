039908dc503a443774965c61b9527e68bf7d2fb0
ketchadapron <ketchadapron.s@gmail.com>
2023-04-12 21:58:28 -0500

    Added function time zone

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+
+  let chicagoTime = moment().tz("America/Chicago");
+
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
